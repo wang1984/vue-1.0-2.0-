c3f9f09a4440e473ff9ef02733e4b8ff958f3ca0
wang1984 <277920119@qq.com>
2017-08-01 09:51:44 +0800

    jiaoshoujia

---
+++ vue2.0demo/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+     
+      <ul>
+        <li>
+          <router-link to="/home">主页</router-link>
+          <router-link to="/news">新闻</router-link>
+        </li>
+      </ul>
+      <div>
+          <transition enter-active-class="animated zoomInLeft" leave-active-class="animated zoomOutRight">
+             <router-view></router-view>
+          </transition> 
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+ 
+</style>
---
+++ vue2.0demo/src/components/News.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="mynews">
+   	<h3>我是新闻</h3>
+   	 <ul>
+   	 	<li v-for="(val,index) in news">
+   	 		{{val}}
+   	 	</li>
+   	 </ul>
+   </div>
+</template>
+<script>
+	 export default{
+	 	name : 'news',
+	 	data(){
+	 		return{
+	 			news:['111','222','333']
+	 		}
+	 	}
+	 }
+
+</script>
+
+<style scoped>
+	 li{
+		list-style: none;
+		line-height: 30px;
+		font-size: 20px;
+		color: #f60
+	}
+	div{
+		border: 1px solid #000;
+	}
+</style>
